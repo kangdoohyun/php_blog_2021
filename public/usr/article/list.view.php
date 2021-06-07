6c690d4b4741f8fbf364267c98c9ef66d67cf93c
kangdoohyun <rkdengus1208@gmail.com>
2021-06-08 00:29:24 +0900

    디자인 수정

--- public/usr/article/list.view.php
+++ public/usr/article/list.view.php
@@ -1,67 +1,55 @@
 <?php
 $pageTitle = "게시물 리스트";
 ?>
-<script>
-  // function write_authority_check() {
-  //   var session = <?=isset($_SESSION['loginedMemberId']) ? $_SESSION['loginedMemberId'] : 0?>;
-  //   if (session != 0) {
-  //     location.href='./write.php?memberId='+session;
-  //   } else{
-  //     alert('로그인 후 이용해주세요');
-  //   }
-  // }
-  // function make_board_authority_check() {
-  //   var session = <?=isset($_SESSION['loginedMemberId']) ? $_SESSION['loginedMemberId'] : 0?>;
-  //   if (session == 1) {
-  //     location.href='../board/make.php';
-  //   } 
-  //   else if(session == 0) {
-  //     alert('로그인 후 이용해주세요');
-  //   }
-  //   else{
-  //     alert('관리자만 게시판을 생성할 수 있습니다.');
-  //   }
-  // }
-</script>
 <?php require_once __DIR__ . "/../head.php"; ?>
-<section class="con">
+<style>
+  .border-list-box > ul > li{
+    width: calc(100% / <?=count($boards) + 1?>);
+    cursor: pointer;
+    text-align: center;
+    background: #292a2d;
+    box-shadow: 0 3px 10px #fff;
+  }
+</style>
+<section class="section-1 con">
+  <form action="../board/make.php?" method="POST">
+    <input class="input-btn" type="submit" value="게시판 생성">
+  </form>
   <form action="./write.php?" method="POST">
     <input type="hidden" name="memberId" value="<?=$_REQUEST['APP__loginedMemberId']?>">
     <input class="input-btn" type="submit" value="글 작성">
   </form>
-  <form action="../board/make.php?" method="POST">
-    <input class="input-btn" type="submit" value="게시판 생성">
-  </form>
-  <hr>
-  <nav>
-    <ul>
-    <li><a href="./list.php">전체보기</a></li>
+  
+  <nav class="border-list-box">
+    <ul class="inline-grid">
+      <li><a href="./list.php">전체보기</a></li>
       <?php foreach( $boards as $board ) { ?>
-        <li><a href="./list.php?boardId=<?=$board['id']?>"><?=$board['name']?></a></li>
+      <li><a href="./list.php?boardId=<?=$board['id']?>"><?=$board['name']?></a></li>
       <?php } ?>
     </ul>
   </nav>
-  <div class="inline-grid list-content-box">
-    <span>번호</span>
-    <span>제목</span>
-    <span>작성자</span>
-    <span>작성 날짜</span>
+  
+  <div class="list-content-box img-box">
+    <ul class="inline-grid">
+      <?php foreach( $articles as $article ) { ?>
+      <?php $member =$this->memberService->getMemberById($article['memberId'])?>
+      <li>
+        <div class="img-box">
+          <!-- 이미지 -->
+          <img src="programming-3170991.png" alt="">
+          <!-- 번호 -->
+          <!-- <p><?=$article['id']?></p> -->
+          <!-- 제목 -->
+          <p><a href="./detail.php?id=<?=$article['id']?>"><?=$article['title']?></a></p>
+          <!-- 작성자 -->
+          <!-- <p><?=$member['nickname']?></p> -->
+          <!-- 작성 날짜 -->
+          <!-- <p><?=$article['regDate']?></p> -->
+        </div>
+      </li>
+      <?php } ?>
+    </ul>
   </div>
-  <hr>
-  <?php foreach( $articles as $article ) { ?>
-    <?php $member =$this->memberService->getMemberById($article['memberId'])?>
-    <div class="inline-grid list-content-box">
-      <!-- 번호 -->
-      <span><?=$article['id']?></span>
-      <!-- 제목 -->
-      <span><a href="./detail.php?id=<?=$article['id']?>"><?=$article['title']?></a></span>
-      <!-- 작성자 -->
-      <span><?=$member['nickname']?></span>
-      <!-- 작성 날짜 -->
-      <span><?=$article['regDate']?></span>
-    </div>
-  <?php } ?>
 </section>
 
 <?php require_once __DIR__ . "/../foot.php"; ?>
- 
--- public/usr/head.php
+++ public/usr/head.php
@@ -10,32 +10,32 @@ $loginedMember = $_REQUEST['APP__loginedMember'];
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?=$pageTitle?></title>
-  <link rel="stylesheet" href="/common.css?ver=1">
+  <link rel="stylesheet" href="/common.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
   <header>
-    <nav class="top-bar-menu-box">
-      <ul class="inline-grid">
+    <nav class="top-bar-menu-box con">
+      <ul class="inline-grid text-right">
         <?php if ($isLogined) { ?>
           <li>
-            <a class="text-border" href="../member/mypage.php">
+            <a href="../member/mypage.php">
               <span><i class="fas fa-user-cog"></i></span>
             </a>
           </li>
           <li>
-            <a class="text-border" href="../member/doLogout.php">
+            <a href="../member/doLogout.php">
               <span><i class="fas fa-sign-out-alt"></i></span>
             </a>
           </li>
         <?php } else{ ?>
           <li>
-            <a class="text-border" href="../member/join.php">
+            <a href="../member/join.php">
               <span><i class="fas fa-user-plus"></i></span>
             </a>
           </li>
           <li>
-            <a class="text-border" href="../member/login.php">
+            <a href="../member/login.php">
               <span><i class="fas fa-sign-in-alt"></i></span>
             </a>
           </li>
@@ -43,7 +43,7 @@ $loginedMember = $_REQUEST['APP__loginedMember'];
       </ul>
     </nav>
     <div>
-      <h1 class="text-border page-name"><?=$pageTitle?></h1>
+      <h1 class="page-name"><?=$pageTitle?></h1>
     </div>
   </header>
   
