b0be63debdb706425e4e9464f0b19c6ac2ccf1cb
kangdoohyun <rkdengus1208@gmail.com>
2021-06-09 00:10:10 +0900

    도메인만 입력할 경우, 값이 없어서 접속이 안되던 오류 해결

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
 require_once __DIR__ . "/../webinit.php";
 
-// jsLocationReplaceExit("/usr/member/login.php");
 list($action) = explode('?', $_SERVER['REQUEST_URI']);
 
 $action = substr($action, 1);
 
-App__run($action);
+APP__run($action);
